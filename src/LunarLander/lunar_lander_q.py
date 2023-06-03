fb532b34a0570715955c3bb871b213ae7cb26d8b
mfrankic <marin.frankic@eglas.hr>
2023-06-03 19:40:34 +0200

    lunar lander q-learning

---
+++ src/LunarLander/lunar_lander_q.py
@@ -0,0 +1,100 @@
+import gymnasium as gym
+import collections
+import numpy as np
+import random
+
+env = gym.make("LunarLander-v2")
+env.reset()
+
+n_episodes = 50000
+gamma = 0.99
+lr = 0.1
+min_eps = 0.01
+
+print_freq = 100
+
+q_states = collections.defaultdict(float)   # note that the first insertion of a key initializes its value to 0.0
+return_per_ep = [0.0]
+epsilon = 1.0
+num_actions = env.action_space.n
+
+def discretize_state(state):
+    discrete_state = (min(2, max(-2, int((state[0]) / 0.05))), \
+                        min(2, max(-2, int((state[1]) / 0.1))), \
+                        min(2, max(-2, int((state[2]) / 0.1))), \
+                        min(2, max(-2, int((state[3]) / 0.1))), \
+                        min(2, max(-2, int((state[4]) / 0.1))), \
+                        min(2, max(-2, int((state[5]) / 0.1))), \
+                        int(state[6]), \
+                        int(state[7]))
+
+    return discrete_state
+
+def epsilon_greedy(q_func, state, eps, env_actions):
+    prob = np.random.random()
+
+    if prob < eps:
+        return random.choice(range(env_actions))
+    else:
+        qvals = [q_func[state + (action, )] for action in range(env_actions)]
+        return np.argmax(qvals)
+      
+def greedy(qstates_dict, state, env_actions):
+    qvals = [qstates_dict[state + (action, )] for action in range(env_actions)]
+    return max(qvals)
+
+def decay_epsilon(curr_eps, exploration_final_eps):
+    if curr_eps < exploration_final_eps:
+        return curr_eps
+    
+    return curr_eps * 0.996
+
+for i in range(n_episodes):
+    t = 0
+
+    # Initial episode state: S
+    curr_state = discretize_state(env.reset()[0])
+    
+    while True:
+        # choose action A from S using behaviour policy -> ε-greedy
+        action = epsilon_greedy(q_states, curr_state, epsilon, num_actions)
+
+        # Create (S, A) pair
+        qstate = curr_state + (action, )
+
+        # Take action A, earn immediate reward R and land into next state S'
+        # S --> A --> R --> S'
+        observation, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        next_state = discretize_state(observation)
+
+        ###################################################################
+        # Policy evaluation step
+        if not done:
+            q_states[qstate] += lr * (reward + gamma * greedy(q_states, next_state, num_actions) - q_states[qstate]) # (S', A') non terminal state
+        else:
+            q_states[qstate] += lr * (reward - q_states[qstate])    # (S', A') terminal state
+        ###################################################################
+
+        return_per_ep[-1] += reward
+
+        if done:
+            if (i + 1) % print_freq == 0:
+                print("\nEpisode finished after {} timesteps".format(t + 1))
+                print("Episode {}: Total Return = {}".format(i + 1, return_per_ep[-1]))
+                print("Total keys in q_states dictionary = {}".format(len(q_states)))
+
+            if (i + 1) % 100 == 0:
+                mean_100ep_reward = round(np.mean(return_per_ep[-101:-1]), 1)
+                print("Last 100 episodes mean reward: {}".format(mean_100ep_reward))
+
+            epsilon = decay_epsilon(epsilon, min_eps)
+            return_per_ep.append(0.0)
+
+            break
+
+        curr_state = next_state
+        t += 1
+    
+# save q_states dictionary to file
+np.save("q_states.npy", q_states)
---
+++ src/LunarLander/lunar_lander_q_play.py
@@ -0,0 +1,56 @@
+import gymnasium as gym
+import numpy as np
+
+# Load the saved q_states dictionary
+q_states = np.load("q_states.npy", allow_pickle=True).item() 
+
+# Define a function for discretizing states similar to the training script
+def discretize_state(state):
+    discrete_state = (min(2, max(-2, int((state[0]) / 0.05))), \
+                        min(2, max(-2, int((state[1]) / 0.1))), \
+                        min(2, max(-2, int((state[2]) / 0.1))), \
+                        min(2, max(-2, int((state[3]) / 0.1))), \
+                        min(2, max(-2, int((state[4]) / 0.1))), \
+                        min(2, max(-2, int((state[5]) / 0.1))), \
+                        int(state[6]), \
+                        int(state[7]))
+    return discrete_state
+
+# Define a function to select the best action for a given state
+def best_action(qstates_dict, state, env_actions):
+    qvals = [qstates_dict[state + (action, )] for action in range(env_actions)]
+    return np.argmax(qvals)
+
+env = gym.make("LunarLander-v2", render_mode="human")
+env.reset()
+
+# This is the maximum number of timesteps per episode. Set it as per your requirements.
+max_timesteps_per_episode = 1000
+
+# Loop over episodes
+for episode in range(1):
+    # Reset the environment and initialize variables
+    state = discretize_state(env.reset()[0])
+    total_reward = 0
+
+    for t in range(max_timesteps_per_episode):
+        # Choose the action with the highest Q-value for the current state
+        action = best_action(q_states, state, env.action_space.n)
+
+        # Execute the action and get the next state and reward
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        done = terminated or truncated
+        next_state = discretize_state(next_state)
+
+        total_reward += reward
+
+        # If the episode is finished, we leave the for loop
+        if done:
+            break
+
+        state = next_state
+
+    print("\nEpisode {} finished after {} timesteps".format(episode + 1, t + 1))
+    print("Episode {}: Total Reward = {}".format(episode + 1, total_reward))
+
+env.close()
